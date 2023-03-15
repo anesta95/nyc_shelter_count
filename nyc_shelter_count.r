732974b590b199432f03a0565b83b98e961654e2
anesta95 <adriannesta@gmail.com>
2023-03-15 10:07:51 -0400

    refining function to get DHS pdf table data

--- nyc_shelter_count.r
+++ nyc_shelter_count.r
@@ -10,28 +10,62 @@ library(tidyr)
 
 # TODO: Write automated pdf downloader with curl() or download.file() with
 # url: https://www1.nyc.gov/assets/dhs/downloads/pdf/dailyreport.pdf
-daily_report <- pdf_text("dailyreport.pdf") %>%
+daily_report <- pdf_text("dailyreport_2022-03-13.pdf") %>%
   nth(1) 
 
 # Extracting the report date
-date <- str_extract(daily_report, "\\w+\\s+\\d{1,2},\\s+\\d{4}") %>% 
-  strptime(format = "%B %d, %Y", tz = "UTC")
+report_date <- str_extract(daily_report, "\\w+\\s+\\d{1,2},\\s+\\d{4}") %>% 
+  base::as.Date(format = "%B %d, %Y")
 
 # TODO: This at times returns "ghost" columns with no data. Will have to write code to 
 # filter them out.
-latest_dhs <- extract_tables("dailyreport.pdf")
+latest_dhs <- extract_tables("dailyreport_2022-03-13.pdf")
 
 table_names <- c("FAMILY INTAKE", "TOTAL SHELTER CENSUS",
                  "Total Single Adults", "FAMILIES WITH CHILDREN",
                  "ADULT FAMILIES CENSUS")
 
+clean_tbls_3_6 <- function(table_name, list) {
+  
+  idx <- detect_index(list, ~any(str_detect(.x, table_name)))
+  
+  matrix <- list[[idx]]
+  
+  partial_df <- as.data.frame(matrix, stringsAsFactors = F) %>% 
+                row_to_names(row_number = 1) %>% 
+                clean_names()
+  
+  if (table_name == "Total Single Adults") {
+    df <- partial_df %>% 
+          rename(measure = x, count = single_adults) %>% 
+          mutate(table = make_clean_names(table_name),
+                 date = report_date)
+    
+  } else {
+    col_name <- sym(make_clean_names(table_name))
+    
+    df <- partial_df %>% 
+      select(col_name) %>% 
+      separate(col = col_name, into = c("measure", "count"),
+               sep = "\\s+(?=\\d)") %>% 
+      mutate(table = make_clean_names(table_name),
+             date = report_date)
+  }
+  
+  return(df)
+  
+}
+
 
 extract_dhs_daily_data <- function(table_name, list) {
   
   if (table_name == "FAMILY INTAKE") {
-    # TODO: Now grabbing by table dimentions need to decide most optimal way based on
+    # Note: Now grabbing by table dimentions. We need to decide most optimal way based on
     # returns from extract_tables()
-    idx <- detect_index(list, ~all(dim(.x) == c(11, 5)))
+    # idx <- detect_index(list, ~all(dim(.x) == c(11, 5)))
+    # Now just looking for the 11 row tibble since FAMILY INTAKE can show up in table 1 and two
+    # and sometimes correct table has 5 columns or six columns
+    idx <- detect_index(list, ~nrow(.x) == 11)
     
     matrix <- list[[idx]]
     
@@ -41,6 +75,9 @@ extract_dhs_daily_data <- function(table_name, list) {
     single_adults_row <- which(safi_initial_df$V1 == "SINGLE ADULTS")
     family_intake_row <- which(safi_initial_df$V4 == "FAMILY INTAKE")
     
+    ## TODO: This still needs some reworking based on the variable number of columns 
+    ## the first table is returned with after extract_tables(). Might not need this check and check
+    ## in next if block below for columns 1 & 4
     if (single_adults_row != family_intake_row) {
       stop(simpleError("SINGLE ADULTS and FAMILY INTAKE rows are not aligned."))
     }
@@ -51,10 +88,8 @@ extract_dhs_daily_data <- function(table_name, list) {
       row_to_names(row_number = 1) %>% 
       clean_names() %>% 
       rename(single_adults_count = x, sites_reported = x_2, family_intake_count = x_3)
-            
-    ### Stopped here  
-                    
     
+    ## Second check: is this needed?
     if (all(names(cleaned_safi)[c(1, 4)] == c("single_adults", "family_intake"))) {
       cj_count <- cleaned_safi %>% 
         pull(single_adults_count) %>% 
@@ -67,56 +102,33 @@ extract_dhs_daily_data <- function(table_name, list) {
       
       sa_cj <- sa_no_cj %>% 
         bind_rows(tibble(single_adults = "Criminal Justice Short-term Housing",
-                         single_adults_count = cj_count))
+                         single_adults_count = cj_count)) %>% 
+        rename(measure = single_adults, count = single_adults_count) %>% 
+        mutate(table = "single_adults", date = report_date)
       
-      fi_no_cj <- cleaned_safi %>% 
+      fi_no_sa <- cleaned_safi %>% 
         slice(1:4) %>% 
-        select(family_intake, family_intake_count)
+        select(family_intake, family_intake_count) %>% 
+        rename(measure = family_intake, count = family_intake_count) %>% 
+        mutate(table = "family_intake", date = report_date)
       
+      df <- bind_rows(sa_cj, fi_no_sa)
       
+    } else {
+      stop(simpleError("SINGLE ADULTS and FAMILY INTAKE column placement misaligned"))
     }
     
-    
+  } else {
+    df <- clean_tbls_3_6(table_name = table_name, list = list)
     
   }
   
-  
+  return(df)
   
 }
 
+map_dfr(table_names, ~extract_dhs_daily_data(.x, latest_dhs))
 
-
-detect_index(latest_dhs, ~any(str_detect(.x, "FAMILY INTAKE"))) -> single_adults_family_intake_index
-detect_index(latest_dhs, ~all(dim(.x) == c(11, 5))) -> single_adults_family_intake_index
-
-latest_dhs[[single_adults_family_intake_index]] -> single_adults_family_intake_matrix
-
-as.data.frame(single_adults_family_intake_matrix,
-              stringsAsFactors = F) %>% 
-  row_to_names(row_number = 1) %>% 
-  clean_names() %>% 
-  rename(single_adults_count = x, sites_reported = x_2, family_intake_count = x_3) -> cleaned_safi 
-
-if (all(names(cleaned_safi)[c(1, 4)] == c("single_adults", "family_intake"))) {
-  cj_count <- cleaned_safi %>% 
-    pull(single_adults_count) %>% 
-    stri_remove_empty() %>% 
-    last()
-  
-  sa_no_cj <- cleaned_safi %>% 
-    slice(1:7) %>% 
-    select(single_adults, single_adults_count)
-  
-  sa_cj <- sa_no_cj %>% 
-    bind_rows(tibble(single_adults = "Criminal Justice Short-term Housing",
-                     single_adults_count = cj_count))
-  
-  fi_no_cj <- cleaned_safi %>% 
-    slice(1:4) %>% 
-    select(family_intake, family_intake_count)
-  
-
-}
 
 detect_index(latest_dhs, ~any(str_detect(.x, "TOTAL SHELTER CENSUS"))) -> tsc_index
 
@@ -176,7 +188,38 @@ cleaned_afc <- as.data.frame(afc_matrix,
 
 
 
-
+# Old SINGLE ADULTS and FAMILY INTAKE table method
+# detect_index(latest_dhs, ~any(str_detect(.x, "FAMILY INTAKE"))) -> single_adults_family_intake_index
+# detect_index(latest_dhs, ~all(dim(.x) == c(11, 5))) -> single_adults_family_intake_index
+# 
+# latest_dhs[[single_adults_family_intake_index]] -> single_adults_family_intake_matrix
+# 
+# as.data.frame(single_adults_family_intake_matrix,
+#               stringsAsFactors = F) %>% 
+#   row_to_names(row_number = 1) %>% 
+#   clean_names() %>% 
+#   rename(single_adults_count = x, sites_reported = x_2, family_intake_count = x_3) -> cleaned_safi 
+# 
+# if (all(names(cleaned_safi)[c(1, 4)] == c("single_adults", "family_intake"))) {
+#   cj_count <- cleaned_safi %>% 
+#     pull(single_adults_count) %>% 
+#     stri_remove_empty() %>% 
+#     last()
+#   
+#   sa_no_cj <- cleaned_safi %>% 
+#     slice(1:7) %>% 
+#     select(single_adults, single_adults_count)
+#   
+#   sa_cj <- sa_no_cj %>% 
+#     bind_rows(tibble(single_adults = "Criminal Justice Short-term Housing",
+#                      single_adults_count = cj_count))
+#   
+#   fi_no_cj <- cleaned_safi %>% 
+#     slice(1:4) %>% 
+#     select(family_intake, family_intake_count)
+#   
+# 
+# }
 
 
 
