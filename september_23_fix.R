a64219bde1dc133d638f0a35993bd434a6cd25b1
Patrick Spauster <patrick.spauster@gmail.com>
2023-09-14 21:00:57 -0400

    okay close

--- september_23_fix.R
+++ september_23_fix.R
@@ -1,16 +1,21 @@
 #DONT RUN THIS AGAIN! I TOOK OUT THE BAD DATES
 
-# dhs_unhoused_report <- read_csv("./data/dhs_daily_report.csv",
+# dhs_unhoused_report_7.4.23 <- read_csv("./data/dhs_daily_report.csv",
 #                               col_names = T,
-#                               col_types = "cdcD")%>%
-#   filter(date <= as.Date("2023-07-01")) %>% 
-#   write_csv("./data/dhs_daily_report.csv")
+#                               col_types = "cdcD") %>%
+#   filter(date <= as.Date("2023-07-01"))
+#   
+# write_csv(dhs_unhoused_report_7.4.23, "./data/dhs_daily_report.csv")
 
 keepdates <- seq(as.Date("2023-07-02"), as.Date("2023-09-12"), by="days")
 
 files <- list.files(path = "dhs_daily_report_unhoused_report_pdfs")
 
 filtered_dates <- keepdates[keepdates %in% str_sub(files,0,10)]
+
+table_names <- c("FAMILY INTAKE", "TOTAL SHELTER CENSUS",
+                 "Total Single Adults", "FAMILIES WITH CHILDREN",
+                 "ADULT FAMILIES CENSUS")
 
 # this needs the functions loaded from nyc_shelter_count to run
 # libraries too
@@ -20,16 +25,24 @@ dhs_unhoused_report <- read_csv("./data/dhs_daily_report.csv",
                                   col_names = T,
                                   col_types = "cdcD")
 
+print(dhs_unhoused_report)
+
 latest_dhs <- extract_tables(paste0("./dhs_daily_report_unhoused_report_pdfs/", report_date, "_daily_report.pdf"))
 
+print(latest_dhs)
+
+#this fails - I think because we can't pass report_date down to the other nested functions that need it
+#since it's not a global in this script like it is in the main script
 dhs_unhoused_report_new <- map_dfr(table_names, ~extract_dhs_daily_data(.x, latest_dhs)) %>% 
   mutate(count = as.numeric(str_remove_all(count, ",")))
 
 dhs_unhoused_report_full <- bind_rows(dhs_unhoused_report_new, dhs_unhoused_report)
 # Write to disk if new data
-write_csv(dhs_unhoused_report_full, "./data/dhs_daily_report.csv")
+write_csv(dhs_unhoused_report_full, "./data/dhs_daily_report1.csv")
 
 }
+
+fill_in_report("2023-07-02")
 
 walk(filtered_dates, .f = ~fill_in_report(.x))
 
